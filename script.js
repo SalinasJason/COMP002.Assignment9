5d10dc988b7c56434a415897619a3ad7bbc3476c
SalinasJason <jasonsalinas770@gmail.com>
2024-12-01 17:47:08 -0800

    added the answers

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("A: The videos from linkedin really worked well for me, it helped me learn more about the topic that we were currently on that week.");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("A:The only thing that I will change will be the learning pace, it was a bit too fast.");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("A: Yes, what I expected to learn was about JavaScript and of course that's what I got.");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("A: All the concepts were new to me and I have never encountered any in the past.");
